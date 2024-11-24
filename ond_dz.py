94bc3dfe79c11cc23894ec0b693d40879217ed89
git2004 <48566560+git2004@users.noreply.github.com>
2024-11-25 02:32:02 +0300

    Update ond_dz.py

--- ond_dz.py
+++ ond_dz.py
@@ -154,28 +154,6 @@ Mi =[
     100.204, 28.0135, 44.01, 4.0026, 2.0159
 ]
 
-#Подсчет xi по формуле (18)
-numerators = [r / z for r, z in zip(ri, zci)]
-denominator = sum(numerators)
-xi = [num / denominator for num in numerators]
-print(xi)
-
-Kp1=9.80665*10**(-2)
-Kp2=1.33322*10**(-4)
-pressure = 18 #исходное абсолютное давление природного газа в МПа
-temperature = 3
-T=temperature+273.15 #Температура природного газа в К
-
-import numpy as np
-
-# Given data
-xi = np.array([
-    0.8486487705378545, 0.06598954882315745, 0.020685434562408678, 0.0, 
-    0.0028903604523203012, 0.0, 0.0, 0.0007978080977642223, 0.0, 
-    0.0008639586324170732, 0.0, 0.05003061595948862, 0.00968620464280331, 
-    0.0004072982917858287, 0.0
-])
-
 Ki = np.array([
     0.4619255, 0.5279209, 0.583749, 0.6406937, 0.6341423,
     0.6738577, 0.6738577, 0.6798307, 0.7175118, 0.7175118,
@@ -199,38 +177,6 @@ Kij = np.array([
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.982361, 1, 1, 1],
     [1.02326, 1.02034, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.03227, 1, 1, 1]
 ])
-
-#Подсчет смесевого параметра Kx по формуле (5)
-Nc = len(xi)
-sum_xi_Ki_52 = np.sum(xi * Ki**(5/2))
-term1 = sum_xi_Ki_52**2
-term2 = 0
-for i in range(Nc - 1):
-    for j in range(i + 1, Nc):
-        Kij_52 = (Kij[i, j]**5 - 1) * (Ki[i] * Ki[j])**(5/2)
-        term2 += 2 * xi[i] * xi[j] * Kij_52
-Kx = (term1 + term2)**(1/5)
-print('Kx=', Kx)
-
-#Подсчет давления нормировки p0m по формуле (4)
-R = 8.31451
-Lt = 1      
-p0m = 10**-3 * Kx**-3 * R * Lt
-print('p0m',p0m)
-
-#Подсчет молярной массы Mm по формуле (19) 
-Nc = len(Mi)
-Mm = sum(Mi[i] * xi[i] for i in range(Nc))
-print(Mm)
-
-#Подсчет Gij по формуле (17) 
-N = len(Gi)
-G_ij = np.zeros((N, N))
-for i in range(N):
-    for j in range(N):
-        G_ij[i, j] = Gij[i][j] * 0.5 * (Gi[i] + Gi[j]) 
-print('Gij',G_ij)
-
 Ei = [
     151.3183,
     244.1667,
@@ -266,17 +212,6 @@ Eij = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1.17052, 1.16446, 1.034787, 1.3, 1.3, 1, 1, 1, 1, 1, 1, 1.08632, 1.28179, 1, 1]
 ]
-
-#Подсчет Eij по формуле (16) 
-Ei = np.array(Ei)
-Eij = np.array(Eij)
-N = len(Ei)
-E_ij = np.zeros((N, N))
-for i in range(N):
-    for j in range(N):
-        E_ij[i, j] = Eij[i, j] * np.sqrt(Ei[i] * Ei[j])
-print('Eij', E_ij)
-
 Gi = [
     0,
     0.0793,
@@ -323,141 +258,6 @@ w = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 Si = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 Wi = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-#Подсчет Bnij по формуле (15) 
-N = len(Gi)
-B_nij = np.zeros((N, N))
-for i in range(N):
-    for j in range(N):
-        B_nij[i, j] = (Gij[i, j] + 1 - g[i] ) * (Qi[i] * Qi[j] + 1 - q[i]) * (np.sqrt(Fi[i] * Fi[j] + 1 - f[i])) * (Si[i] * Si[j] + 1 - Si[i]) * (Wi[i] * Wi[j] + 1 - w[i])
-print('Bij', B_nij)
-
-import numpy as np
-
-xi = np.array([8.48648771e-01, 6.59895488e-02, 2.06854346e-02, 0.00000000e+00,
-               2.89036045e-03, 0.00000000e+00, 0.00000000e+00, 7.97808098e-04,
-               0.00000000e+00, 8.63958632e-04, 0.00000000e+00, 5.00306160e-02,
-               9.68620464e-03, 4.07298292e-04, 0.00000000e+00])
-
-Ei = np.array([
-    151.3183,
-    244.1667,
-    298.1183,
-    324.0689,
-    337.6389,
-    365.5999,
-    365.5999,
-    370.6823,
-    402.636293,
-    402.636293,
-    402.636293,
-    99.73778,
-    241.9606,
-    2.610111,
-    26.95794
-])
-
-Eij = np.array([
-    [1, 1, 0.994635, 1.01953, 0.989844, 1, 1.00235, 0.999268, 1, 1.107274, 1.107274, 0.97164, 0.960644, 1, 1.17052],
-    [1, 1, 1.02256, 1, 1.01306, 1, 1, 1.00532, 1, 1, 1, 0.97012, 0.925053, 1, 1.16446],
-    [0.994635, 1.02256, 1, 1, 1.0049, 1, 1, 1, 1, 1, 1, 0.945939, 0.960237, 1, 1.034787],
-    [1.01953, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.946914, 0.906849, 1, 1.3],
-    [0.989844, 1.01306, 1.0049, 1, 1, 1, 1, 1, 1, 1, 1, 0.973384, 0.897362, 1, 1.3],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1.00235, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.95934, 0.726255, 1, 1],
-    [0.999268, 1.00532, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.94552, 0.859764, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1.107274, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.855134, 1, 1],
-    [1.107274, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.855134, 1, 1],
-    [0.97164, 0.97012, 0.945939, 0.946914, 0.973384, 1, 0.95934, 0.94552, 1, 1, 1, 1, 1.02274, 1, 1.08632],
-    [0.960644, 0.925053, 0.960237, 0.906849, 0.897362, 1, 0.726255, 0.859764, 1, 0.855134, 0.855134, 1.02274, 1, 1, 1.28179],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1.17052, 1.16446, 1.034787, 1.3, 1.3, 1, 1, 1, 1, 1, 1, 1.08632, 1.28179, 1, 1]
-])
-
-Vij = np.array([
-    [1, 1, 0.990877, 1, 0.992291, 1, 1, 1.00367, 1, 1.302576, 1.302576, 0.886106, 0.963827, 1, 1.15639],
-    [1, 1, 1.065173, 1.25, 1.25, 1, 1.25, 1.25, 1, 1, 1, 0.816431, 0.96987, 1, 1.61666],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.915502, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.993556, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.066638, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.066638, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.835058, 1, 0.408838],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-])
-
-#Подсчет параметра V по формуле (14)
-Nc = len(xi)
-term1 = np.sum(xi * Ei**(5/2))**2
-term2 = 0
-for i in range(Nc - 1):
-    for j in range(i + 1, Nc):
-        term2 += xi[i] * xi[j] * (Vij[i, j]**5 - 1) * (Ei[i] * Ei[j])**(5/2)
-V = (term1 + 2 * term2)**(1/5)
-print(f"V = {V}")
-
-#Подсчет параметра F по формуле (13)
-F = 0
-for i in range(len(xi)):
-    F += xi[i]**2 * Fi[i]
-print(f"F = {F}")
-
-#Подсчет параметра Q по формуле (12)
-Q = 0
-for i in range(len(xi)):
-    Q += xi[i] * Qi[i]
-print(f"Q = {Q}")
-
-
-xi = np.array([
-    0.8486487705378545, 0.06598954882315745, 0.020685434562408678, 0.0, 
-    0.0028903604523203012, 0.0, 0.0, 0.0007978080977642223, 0.0, 
-    0.0008639586324170732, 0.0, 0.05003061595948862, 0.00968620464280331, 
-    0.0004072982917858287, 0.0
-])
-
-Gi = np.array([
-    0,
-    0.0793,
-    0.141239,
-    0.256692,
-    0.281835,
-    0.332267,
-    0.332267,
-    0.366911,
-    0.289731,
-    0.289731,
-    0.289731,
-    0.027815,
-    0.189065,
-    0,
-    0.034369
-])
-
-Gij = np.array([
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.807653, 1, 1.95731],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.370296, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.982746, 1, 1],
-    [0.807653, 0.370296, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.982746, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1.95731, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-])
-
 Gij_corrected = np.array([
     [0, 0.03965, 0.0706195, 0.128346, 0.1409175, 0.1661335, 0.1661335, 0.1834555, 0.1448655, 0.1448655, 0.1448655, 0.0139075, 0.07634946, 0, 0.03363539],
     [0.03965, 0.0793, 0.1102695, 0.167996, 0.1805675, 0.2057835, 0.2057835, 0.2231055, 0.1845155, 0.1845155, 0.1845155, 0.0535575, 0.04968724, 0.03965, 0.0568345],
@@ -476,17 +276,28 @@ Gij_corrected = np.array([
     [0.03363539, 0.0568345, 0.087804, 0.1455305, 0.158102, 0.183318, 0.183318, 0.20064, 0.16205, 0.16205, 0.16205, 0.031092, 0.111717, 0.0171845, 0.034369]
 ])
 
-#Подсчет параметра G по формуле (11)
-Nc = len(xi)
-term1 = np.sum(xi * Gi)
-term2 = 0
-for i in range(Nc - 1):
-    for j in range(i + 1, Nc):
-        term2 += xi[i] * xi[j] * (Gij_corrected[i, j] -1) * (Gi[i] + Gi[j])
-G = term1 + term2
-print(f"G = {G}")
 
+#Подсчет xi по формуле (18)
+numerators = [r / z for r, z in zip(ri, zci)]
+denominator = sum(numerators)
+xi = [num / denominator for num in numerators]
+#print(xi)
 
+Kp1=9.80665*10**(-2)
+Kp2=1.33322*10**(-4)
+pressure = 18 #исходное абсолютное давление природного газа в МПа
+temperature = 3
+T=temperature+273.15 #Температура природного газа в К
+
+import numpy as np
+
+# Given data
+xi = np.array([
+    0.8486487705378545, 0.06598954882315745, 0.020685434562408678, 0.0, 
+    0.0028903604523203012, 0.0, 0.0, 0.0007978080977642223, 0.0, 
+    0.0008639586324170732, 0.0, 0.05003061595948862, 0.00968620464280331, 
+    0.0004072982917858287, 0.0
+])
 
 Bij = np.array([
     [2.000000, 2.000000, 2.000000, 2.000000, 2.000000, 2.000000, 
@@ -537,125 +348,199 @@ Bij = np.array([
 ])
 
 
+#Подсчет смесевого параметра Kx по формуле (5)
+Nc = len(xi)
+sum_xi_Ki_52 = np.sum(xi * Ki**(5/2))
+term1 = sum_xi_Ki_52**2
+term2 = 0
+for i in range(Nc - 1):
+    for j in range(i + 1, Nc):
+        Kij_52 = (Kij[i, j]**5 - 1) * (Ki[i] * Ki[j])**(5/2)
+        term2 += 2 * xi[i] * xi[j] * Kij_52
+Kx = (term1 + term2)**(1/5)
+print('Kx=', Kx)
+
+#Подсчет давления нормировки p0m по формуле (4)
+R = 8.31451
+Lt = 1      
+p0m = 10**-3 * Kx**-3 * R * Lt
+print('p0m',p0m)
+#Расчет приведенного давления pi по формуле (2)
+pi_=pressure/p0m
+print('pi=', pi_)
+
+#Подсчет молярной массы Mm по формуле (19) 
+Nc = len(Mi)
+Mm = sum(Mi[i] * xi[i] for i in range(Nc))
+#print(Mm)
+
+#Подсчет Gij по формуле (17) 
+N = len(Gi)
+G_ij = np.zeros((N, N))
+for i in range(N):
+    for j in range(N):
+        G_ij[i, j] = Gij[i][j] * 0.5 * (Gi[i] + Gi[j]) 
+#print('Gij',G_ij)
+
+#Подсчет параметра G по формуле (11)
+Nc = len(xi)
+term1 = np.sum(xi * Gi)
+term2 = 0
+for i in range(Nc - 1):
+    for j in range(i + 1, Nc):
+        term2 += xi[i] * xi[j] * (Gij_corrected[i, j] -1) * (Gi[i] + Gi[j])
+G = term1 + term2
+print(f"G = {G}")
+
+
+#Подсчет Eij по формуле (16) 
+Ei = np.array(Ei)
+Eij = np.array(Eij)
+N = len(Ei)
+E_ij = np.zeros((N, N))
+for i in range(N):
+    for j in range(N):
+        E_ij[i, j] = Eij[i, j] * np.sqrt(Ei[i] * Ei[j])
+#print('Eij', E_ij)
+
+
+#Подсчет Bnij по формуле (15) 
+N = len(Gi)
+B_nij = np.zeros((N, N))
+for i in range(N):
+    for j in range(N):
+        B_nij[i, j] = (Gij[i, j] + 1 - g[i] ) * (Qi[i] * Qi[j] + 1 - q[i]) * (np.sqrt(Fi[i] * Fi[j] + 1 - f[i])) * (Si[i] * Si[j] + 1 - Si[i]) * (Wi[i] * Wi[j] + 1 - w[i])
+#print('Bij', B_nij)
+
+import numpy as np
+
+#Подсчет параметра V по формуле (14)
+Nc = len(xi)
+term1 = 0
+for i in range(Nc):
+    term1 += xi[i] * (Ei[i] ** (5/2))
+term1 = term1 ** 2
+term2 = 0
+for i in range(Nc - 1):
+    for j in range(i + 1, Nc):
+        term2 += xi[i] * xi[j] * ((Vij[i][j] ** 5 - 1) * (Ei[i] * Ei[j]) ** (5/2))
+V = (term1 + 2 * term2) ** (1/5)
+print(f"V = {V}")
+
+#Подсчет параметра F по формуле (13)
+F = 0
+for i in range(len(xi)):
+    F += xi[i]**2 * Fi[i]
+print(f"F = {F}")
+
+#Подсчет параметра Q по формуле (12)
+Q = 0
+for i in range(len(xi)):
+    Q += xi[i] * Qi[i]
+print(f"Q = {Q}")
 
 #Подсчет параметра Bn по формуле (10)
+from math import sqrt, exp, sinh, cosh
 Nc = len(xi)
 Bn = 0
 for i in range(Nc):
     for j in range(Nc):
         Bn += xi[i] * xi[j] * Bij[i, j] * Eij[i, j] * (Kij[i, j]**(3/2))
 print(f"B_n = {Bn}")
-#Подсчет параметра Cn по формуле (9)
-C_n = (G + 1 - g)**g * (Q**2 + 1 - q)**q * (F + 1 - f)**f * V**u
-print('Cn=', C_n)
-
-#Подсчет параметра Dn и Un по формулам (7-8)
-D_n = np.zeros_like(C_n)  
-D_n[:12] = Bn * Kx**-3
-# For 13 <= n <= 18
-D_n[12:18] = Bn * Kx**-3 - C_n[12:18]
-U_n = np.zeros_like(C_n)  
-U_n[12:] = C_n[12:]
-print('D_n=', D_n)
-print('U_n', U_n)
+Cn = lambda n: ((G + 1 - g[n]) ** g[n]) * \
+        ((Q**2 + 1 - q[n]) ** q[n]) * ((F + 1 - f[n]) ** f[n]) * (V ** u[n])
+U = [0] * 12 + [Cn(n) for n in range(12, 58)]
+Bn = lambda n: sum(xi[i] * xi[j] * Bnij(n, i, j) * Eijn(i, j) ** u[n] * ((Ki[i] * Ki[j]) ** (3 / 2)) 
+                    for i in range(len(xi)) for j in range(len(xi)))
+Bnij = lambda n, i, j: ((Gijn(i, j) + 1 - g[n]) ** g[n]) * ((Qi[i] * Qi[j] + 1 - q[n]) ** q[n]) * \
+                    ((sqrt(Fi[i] * Fi[j]) + 1 - f[n]) ** f[n]) * ((Si[i] * Si[j] + 1 - s[n]) ** s[n]) * \
+                    ((Wi[i] * Wi[j] + 1 - w[n]) ** w[n])  
+Eijn = lambda i, j: Eij[i][j] * sqrt(Ei[i] * Ei[j])         
+Gijn = lambda i, j: (Gij[i][j] * (Gi[i] + Gi[j])) / 2
+D = [Bn(n) * (Kx ** -3) for n in range(12)] + \
+    [Bn(n) * (Kx ** -3) - Cn(n) for n in range(12, 18)] + \
+    [0] * 40
 
 #Расчет начального значения приведенной плотности по формуле (41)
-delta_0=(10**3 * pressure * Kx**3 )/(R*T)
+delta_0=(10 ** 3 * pressure * Kx ** 3 ) / (R * T)
 print('delta=',delta_0)
 
 #Расчет значения тау по формуле (3)
 tau=T/Lt
 print('tau=', tau)
 
-#Расчет значения A0 по формуле (6)
-A0 = 0
-for n in range(58):
-    A0 += a[n] * (delta_0**b[n]) * (tau**(-u[n])) * (D_n[n] + (b[n] - c[n] * k[n]) * U_n[n] * np.exp(-c[n] * k[n]))
-print(f"A_0 = {A0}")
+# Исходные данные (пример)
+delta0 = (10 ** 3 * pressure * Kx ** 3) / (R * T)
+reduced_temp=T/Lt
+# Определение функций A
+def calculate_A0(density):
+    return sum(
+        a[n] * density**b[n] * reduced_temp**(-u[n]) * (
+            b[n] * D[n] + (b[n] - c[n] * k[n] * density**k[n]) * U[n] * exp(-c[n] * density**k[n])
+        )
+        for n in range(58)
+    )
 
-#Расчет значения A1 по формуле (23)
-A1 = 0
-for n in range(58):
-    term1 = (b[n] + 1) * b[n] * D_n[n]
-    term2 = ((b[n] - c[n] * k[n]) * (delta_0**k[n]) * (b[n] - c[n] * k[n] * (delta_0**k[n]) + 1 - c[n] * (k[n]**2) * (delta_0**k[n])))
-    term3 = U_n[n] * np.exp(-c[n] * k[n])
-    A1 += a[n] * (delta_0**b[n]) * (tau**(-u[n])) * (term1 + term2 * term3)
-print(f"A_1 = {A1}")
+def calculate_A1(density):
+    return sum(
+        a[n] * density**b[n] * reduced_temp**(-u[n]) * (
+            (b[n] + 1) * b[n] * D[n] + 
+            ((b[n] - c[n] * k[n] * density**k[n]) * (b[n] - c[n] * k[n] * density**k[n] + 1) - 
+             c[n] * k[n]**2 * density**k[n]) * U[n] * exp(-c[n] * density**k[n])
+        )
+        for n in range(58)
+    )
 
-A2 = 0
-A3 = 0
-for n in range(58):
-    A2 += a[n] * (delta_0**b[n]) * (tau**(-u[n])) * (1 - u[n]) * (b[n] * D_n[n] + (b[n] - c[n] * k[n]) * (delta_0**k[n]) * U_n[n] * np.exp(-c[n] * k[n]))
-    A3 += a[n] * (delta_0**b[n]) * (tau**(-u[n])) * u[n] * (1 - u[n]) * (D_n[n] + U_n[n] * np.exp(-c[n] * k[n]))
+def calculate_A2(density):
+    return sum(
+        a[n] * density**b[n] * reduced_temp**(-u[n]) * (1 - u[n]) * (
+            b[n] * D[n] + (b[n] - c[n] * k[n] * density**k[n]) * U[n] * exp(-c[n] * density**k[n])
+        )
+        for n in range(58)
+    )
 
-print(f"A_2 = {A2}")
-print(f"A_3 = {A3}")
+def calculate_A3(density):
+    return sum(
+        a[n] * density**b[n] * reduced_temp**(-u[n]) * u[n] * (1 - u[n]) * (
+            D[n] + U[n] * exp(-c[n] * density**k[n])
+        )
+        for n in range(58)
+    )
 
-#Расчет приведенного давления pi по формуле (2)
-pi=pressure/p0m
-print('pi=', pi)
-tolerance = 1e-6
-iteration_count = 0
-delta_k = 0.8158610794967571
+# Массив функций A
+A = [calculate_A0, calculate_A1, calculate_A2, calculate_A3]
 
-#Итерационный расчет
-while True:
-    # Расчет нового значения Δδ^(k)
-    delta_delta_k = (pi / tau - (1 + A0) * delta_k) / (1 + A1)
-    
-    # Обновление значения δ^(k)
-    delta_k_new = delta_k + delta_delta_k
-    
-    # Расчет приведенного давления π^(k)_расч
-    pi_calculated = delta_k_new * tau * (1 + A0 + A1 * delta_k_new + A2 * delta_k_new**2 + A3 * delta_k_new**3)
-    
-    # Проверка условия завершения итерационного процесса
-    if abs(pi_calculated - pi) / pi < tolerance:
-        break
-    
-    # Обновление переменной для следующей итерации
-    delta_k = delta_k_new
-    iteration_count += 1
+# Функция для расчета приведенного давления
+def calc_reduced_pres(density):
+    return density * reduced_temp * (1 + A[0](density))
 
-# Вывод результатов
-print(delta_k_new, pi_calculated, iteration_count)
+# Итерационный процесс (23-24)
+while abs((calc_reduced_pres(delta0) - pi_) / pi_) > 10**(-6):
+    A0_delta0 = A[0](delta0)
+    A1_delta0 = A[1](delta0)
+    delta0 += ((pi_ / reduced_temp) - (1 + A0_delta0) * delta0) / (1 + A1_delta0)
 
-#Расчет плотности по формуле (18)
-ro=Mm* Kx**-3 * delta_k_new
-print('ro=',ro)
-
-#Расчет коэффициента схимаемости по формуле (20)
-z=1+A0
-print('z=', z)
-
-
-
-theta = 1 / tau
-
-# Инициализация результата
-cp0i = []
-
-# Цикл для поэлементного расчёта
-for i in range(len(B0i)):
-    # Защита от деления на ноль
-    D_term = 0 if D0i[i] == 0 else (D0i[i] * theta / np.sinh(D0i[i] * theta))**2
-    F_term = 0 if F0i[i] == 0 else (F0i[i] * theta / np.cosh(F0i[i] * theta))**2
-    H_term = 0 if H0i[i] == 0 else (H0i[i] * theta / np.sinh(H0i[i] * theta))**2
-    J_term = 0 if J0i[i] == 0 else (J0i[i] * theta / np.cosh(J0i[i] * theta))**2
-
-    # Расчёт c_p0i для текущего компонента
-    cp = B0i[i] + C0i[i] * D_term + E0i[i] * F_term + G0i[i] * H_term + I0i[i] * J_term
-    cp0i.append(cp)
-
-# Преобразуем результат в массив numpy для удобства
-cp0i = np.array(cp0i)
+# Итоговые значения A0, A1, A2, A3
+A0, A1, A2, A3 = A[0](delta0), A[1](delta0), A[2](delta0), A[3](delta0)
 
 # Вывод результатов
-print("Рассчитанные значения cp0i:")
-print(cp0i)
+print(f"A0 = {A0}, A1 = {A1}, A2 = {A2}, A3 = {A3}")
 
-cp0r = np.sum(xi * cp0i)
-print('cp0r=',cp0r)
+print (pi_)
+    # (27)
+z = 1 + A0
+theta = pi_**(-1)
+cp0ri = lambda i: (
+         B0i[i] + 
+        (C0i[i] * ((D0i[i] * theta) / sinh(D0i[i] * theta)) ** 2 if sinh(D0i[i] * theta) != 0 else 0) + 
+        (E0i[i] * ((F0i[i] * theta) / cosh(F0i[i] * theta)) ** 2 if cosh(F0i[i] * theta) != 0 else 0) + 
+        (G0i[i] * ((H0i[i] * theta) / sinh(H0i[i] * theta)) ** 2 if sinh(H0i[i] * theta) != 0 else 0) + 
+        (I0i[i] * ((J0i[i] * theta) / cosh(J0i[i] * theta)) ** 2 if cosh(J0i[i] * theta) != 0 else 0)
+    )
+cp0r = sum(xi[i] * cp0ri(i) for i in range(len(ri)))
+print('cp0r=', cp0r)
 
-adiabat=((1+A1+(1+A2)**2)/(cp0r-1-A3))/z
-print('adiabat=', adiabat)
+    # (30)
+adiab_ind = (1 + A1 + ((1 + A2) ** 2) / (cp0r - 1 + A3)) / z
+print('k=',adiab_ind)
+
